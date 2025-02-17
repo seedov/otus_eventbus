b68cd1306d0f501d0b1a8f3f8b8dd54b60c90819
antonsedov <antonsedov>
2025-02-18 01:38:48 +0300

    make stats writable

--- Assets/Scripts/Entities/Common/Components/StatsComponent.cs
+++ Assets/Scripts/Entities/Common/Components/StatsComponent.cs
@@ -2,7 +2,7 @@ namespace Lessons.Lesson19_EventBus
 {
     public sealed class StatsComponent
     {
-        public int Strength => _stats.strength.Value;
+        public int Strength {get => _stats.strength.Value; set { _stats.strength.Value = value; } }
         
         private readonly Stats _stats;
         
